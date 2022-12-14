d4fc52562a0697f50710aaeaf7d866de41296041
deniiis9738 <115715797+deniiis9738@users.noreply.github.com>
2022-12-14 15:21:26 +0100

    Update trabajadoresdepartamentos.ps1

--- trabajadoresdepartamentos.ps1
+++ trabajadoresdepartamentos.ps1
@@ -6,8 +6,10 @@ foreach ($group in $file_groups) {
 $file_users=Import-Csv -Path trabajadores.csv
 foreach ($user in $file_users) {
   $clave=ConvertTo-secureString $user.Password -AsPlainText -Force
-  New-LocalUser $user.Password $clave -Description $user.Descripcion -AccountNeverExpires $false -PasswordNeverExpires $false
-  net user $user.Password /logonpasswordchg:yes
-  Add-LocalGroupMember -Group usuarios -Member $user.Password
-  Add-LocalGroupMember -Group $user.Departamentos -Member $user.Password
+  New-LocalUser $user.Cuenta -Password $clave -FullName $user.Nombre -Description $user.Descripcion -AccountNeverExpires $false -PasswordNeverExpires $false
+  net user $user.Cuenta /logonpasswordchg:yes
+  $grupos-usuario = ("usuarios",$user.Departamentos)
+  foreach ($grupo in $grupos-usuario){
+    Add-LocalGroupMember -Group $grupo -Member $user.Cuenta
+  }
 }
